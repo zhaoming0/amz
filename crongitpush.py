6151460c10d0207931c341cb5e8ce48ab26c4fc4
zhaoming0 <71164933@qq.com>
2020-08-07 01:17:19 +0800

    update

---
+++ crongitpush.py
@@ -0,0 +1,24 @@
+from git import Repo
+import os
+import subprocess
+import sys
+import time
+from shutil import copyfile
+
+
+repo = Repo('.')
+while True:
+    time_now = time.strftime("%H:%M:%S", time.localtime())
+    if time_now == "01:18:00":
+        print(time_now)
+        time.sleep(1)
+        for i in (os.listdir('.')):
+            if i.endswith('csv') and i != 'test.csv':
+                print(i)
+                commitName = (i.split('.')[0])
+                copyfile(i, 'test.csv')
+                repo.index.add(['test.csv'])
+                repo.index.commit(commitName)
+                subprocess.check_call(['git', 'push', 'origin', 'master'])
+                time.sleep(10)
+

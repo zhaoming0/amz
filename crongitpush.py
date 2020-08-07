139fb959419d5d65ee9e83338474f1d65e4fc4d7
zhaoming0 <71164933@qq.com>
2020-08-07 21:44:35 +0800

    update ignore

--- crongitpush.py
+++ crongitpush.py
@@ -5,20 +5,21 @@ import sys
 import time
 from shutil import copyfile
 
-
+print(1)
 repo = Repo('.')
 while True:
     time_now = time.strftime("%H:%M:%S", time.localtime())
-    if time_now == "07:00:00":
-        print(time_now)
-        time.sleep(1)
-        for i in (os.listdir('.')):
-            if i.endswith('csv') and i != 'test.csv':
-                print(i)
-                commitName = (i.split('.')[0])
-                copyfile(i, 'test.csv')
-                repo.index.add(['test.csv'])
-                repo.index.commit(commitName)
-                subprocess.check_call(['git', 'push', 'origin', 'master'])
-                time.sleep(10)
+    # if time_now == "21:11:30":
+    print(time_now)
+    time.sleep(1)
+    for i in (os.listdir('.')):
+        print(i)
+        if i.endswith('csv') and i != 'test.csv':
+            print(i)
+            commitName = (i.split('.')[0])
+            copyfile(i, 'test.csv')
+            repo.index.add(['test.csv'])
+            repo.index.commit(commitName)
+            subprocess.check_call(['git', 'push', 'origin', 'master'])
+            time.sleep(10)
 

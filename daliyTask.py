831a4e9867e27887b11c03b7cc41b79f63d7e5d7
zhaoming0 <71164933@qq.com>
2020-08-08 22:20:33 +0800

    update

--- daliyTask.py
+++ daliyTask.py
@@ -16,14 +16,14 @@ while flags:
     if time_now == "07:00:30":
         print(time_now)
         time.sleep(1)
-    for i in (os.listdir(os.path.join(father_path, 'keyword'))):
-        if i.endswith('csv') and i != 'test.csv':
-            print(i + '\n')
-            commitName = (i.split('.')[0])
-            commitName = commitName + ' ' + dates
-            copyfile(os.path.join(father_path, 'keyword', i), 'test.csv')
-            repo.index.add(['test.csv'])
-            repo.index.commit(commitName)
-            subprocess.check_call(['git', 'push', 'origin', 'master'])
-            time.sleep(10)
-    flags = False
+        for i in (os.listdir(os.path.join(father_path, 'keyword'))):
+            if i.endswith('csv'):
+                print(i + '\n')
+                commitName = (i.split('.')[0])
+                commitName = commitName + ' ' + dates
+                copyfile(os.path.join(father_path, 'keyword', i), '..\\test.csv')
+                repo.index.add(['test.csv'])
+                repo.index.commit(commitName)
+                subprocess.check_call(['git', 'push', 'origin', 'master'])
+                time.sleep(10)
+        flags = False
--- keyword/autoLaunch.bat
+++ keyword/autoLaunch.bat
@@ -1,2 +1,5 @@
+@echo off
+
+cd %~dp0
+
 "C:\Users\test\AppData\Local\Programs\Python\Python38\python.exe"  "..\daliyTask.py"
-pause

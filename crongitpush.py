0f763dd7458ece4352c3c287314df51efbcf07b8
zhaoming0 <71164933@qq.com>
2020-08-08 18:57:47 +0800

    update

--- crongitpush.py
+++ crongitpush.py
@@ -4,22 +4,24 @@ import subprocess
 import sys
 import time
 from shutil import copyfile
+current_path=(os.path.abspath(__file__))
+father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".")
 
-print(1)
-repo = Repo('.')
+repo = Repo(os.path.abspath(father_path))
+
 while True:
     time_now = time.strftime("%H:%M:%S", time.localtime())
-    # if time_now == "21:11:30":
-    print(time_now)
-    time.sleep(1)
-    for i in (os.listdir('.')):
-        print(i)
-        if i.endswith('csv') and i != 'test.csv':
-            print(i)
-            commitName = (i.split('.')[0])
-            copyfile(i, 'test.csv')
-            repo.index.add(['test.csv'])
-            repo.index.commit(commitName)
-            subprocess.check_call(['git', 'push', 'origin', 'master'])
-            time.sleep(10)
-
+    dates = time.strftime("%Y-%m-%d", time.localtime())
+    if time_now == "07:00:30":
+        print(time_now)
+        time.sleep(1)
+        for i in (os.listdir('keyword')):
+            if i.endswith('csv') and i != 'test.csv':
+                print(i + '\n')
+                commitName = (i.split('.')[0])
+                commitName = commitName + ' ' + dates
+                copyfile(os.path.join(father_path, 'keyword', i), 'test.csv')
+                repo.index.add(['test.csv'])
+                repo.index.commit(commitName)
+                subprocess.check_call(['git', 'push', 'origin', 'master'])
+                time.sleep(10)

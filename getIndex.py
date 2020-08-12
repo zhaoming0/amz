db96bc0b0d1aa63ca87cb2f033fb16f702951d00
zhaoming0 <71164933@qq.com>
2020-08-12 10:09:43 +0800

    fix null line

--- getIndex.py
+++ getIndex.py
@@ -37,46 +37,47 @@ counts = 0
 final_result = {}
 with open('test.csv','r') as f:
     reader = csv.reader(f)
-    for row in reader:       
-        lineToStr = row[0]
-        lineToList = lineToStr.split(' ')
-        linkStr='s?k='
-        for i in range(len(lineToList)):
-            linkStr= linkStr + lineToList[i] + '+'
-            # picPath = picPath + lineToList[i] +'_'
-        linkStr= linkStr[:-1]
-        # picPath = picPath[:-1].replace('/','-')
-        driver.maximize_window()
-        counts = 1 + counts
-        flags = False
-        keyword = linkStr.replace('+', ' ')[4:]
-        print(str(counts) + ' for  keyword: ' + keyword)
-        if (keyword not in final_result):
-            final_result[keyword] = [0, 0, 0]
-        for i in range(1,7):
-            # print('page :' + str(i))
-            if (final_result[keyword][1] != 0 and final_result[keyword][2] != 0):
-                break
-            driver.get('https://www.amazon.com/' + linkStr + '&page=' + str(i)+ '&language=en_US')            
-            if flags == False:
-                count = driver.find_element_by_xpath('//*[@id="search"]/span/div/span/h1/div/div[1]/div/div/span[1]').text
-                count = count.split(' ')[-3].replace(',','')
-                if final_result[keyword][0] == 0:
-                    final_result[keyword][0] = count
-                flags = True
-            soup = BeautifulSoup(driver.page_source, "html.parser")
-            for asin in soup.find_all(href=re.compile("READY-PARD-")):
-                links = str(asin.get('href'))
-                if (links.startswith('/gp')):
-                    # adver 
-                    adver = links.split('sr_1_')[1].split('_')[0]
-                    if final_result[keyword][1] == 0:
-                        final_result[keyword][1] = adver
-                else:
-                    nature = links.split('sr_1_')[1].split('?')[0]
-                    # nature
-                    if final_result[keyword][2] == 0:
-                        final_result[keyword][2] = nature
+    for row in reader:
+        if (len(row) != 0):       
+            lineToStr = row[0]
+            lineToList = lineToStr.split(' ')
+            linkStr='s?k='
+            for i in range(len(lineToList)):
+                linkStr= linkStr + lineToList[i] + '+'
+                # picPath = picPath + lineToList[i] +'_'
+            linkStr= linkStr[:-1]
+            # picPath = picPath[:-1].replace('/','-')
+            driver.maximize_window()
+            counts = 1 + counts
+            flags = False
+            keyword = linkStr.replace('+', ' ')[4:]
+            print(str(counts) + ' for  keyword: ' + keyword)
+            if (keyword not in final_result):
+                final_result[keyword] = [0, 0, 0]
+            for i in range(1,7):
+                # print('page :' + str(i))
+                if (final_result[keyword][1] != 0 and final_result[keyword][2] != 0):
+                    break
+                driver.get('https://www.amazon.com/' + linkStr + '&page=' + str(i)+ '&language=en_US')            
+                if flags == False:
+                    count = driver.find_element_by_xpath('//*[@id="search"]/span/div/span/h1/div/div[1]/div/div/span[1]').text
+                    count = count.split(' ')[-3].replace(',','')
+                    if final_result[keyword][0] == 0:
+                        final_result[keyword][0] = count
+                    flags = True
+                soup = BeautifulSoup(driver.page_source, "html.parser")
+                for asin in soup.find_all(href=re.compile("READY-PARD-")):
+                    links = str(asin.get('href'))
+                    if (links.startswith('/gp')):
+                        # adver 
+                        adver = links.split('sr_1_')[1].split('_')[0]
+                        if final_result[keyword][1] == 0:
+                            final_result[keyword][1] = adver
+                    else:
+                        nature = links.split('sr_1_')[1].split('?')[0]
+                        # nature
+                        if final_result[keyword][2] == 0:
+                            final_result[keyword][2] = nature
 driver.quit()
 
 for k,v in final_result.items():
